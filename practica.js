d22bd303013474a6b56b881c521729ab97dc24de
bisarroyo <bis.arroyo@gmail.com>
2021-02-07 22:17:02 -0600

    se realiza modificaciones en la estructutra del documento y se cambian estilos

--- practica.js
+++ practica.js
@@ -10,6 +10,10 @@ var tijera = 3;
 
 document.addEventListener("keyup", game);
 
+var win = [];
+var draw = [];
+var loss = [];
+
 function game(key)
 {
     var maquina = parseInt(Math.random() * 3 + 1);
@@ -30,7 +34,7 @@ function logica(user)
 {
     var maquina = parseInt(Math.random() * 3 + 1);
     var select;
-    if (maquina ===1){
+    if (maquina === 1){
         select = "<i class='fas fa-hand-rock icon-1'></i>";
     }else if (maquina === 2){
         select = "<i class='fas fa-hand-paper icon-1'></i>";
@@ -41,18 +45,31 @@ function logica(user)
     {
         document.getElementById("resultado").innerHTML = "Has empatado";
         document.getElementById("machine").innerHTML = select;
+        draw.push(1);
     }else if (
         (user === piedra && maquina === tijera) ||
         (user === papel && maquina === piedra) ||
         (user === tijera && maquina === papel)
         )
     {
-            document.getElementById("resultado").innerHTML = "Has ganado"
-            document.getElementById("machine").innerHTML = select;
+        document.getElementById("resultado").innerHTML = "Has ganado"
+        document.getElementById("machine").innerHTML = select;
+        win.push(1);
     }else
     {
         document.getElementById("resultado").innerHTML = "Has Perdido"
         document.getElementById("machine").innerHTML = select;
+        loss.push(1);
     }
     console.log("la opción de la maquina es: " + maquina);
+    document.getElementById("global").innerHTML = `Ha ganado: ${win.length}, ha perdido: ${loss.length}, ha empatado: ${draw.length}`;
+    switch (true){
+        case (win.length === 3):
+            alert("Has ganado a la máquina");
+        break
+        case (loss.length === 3):
+            alert("Mas suerte para lapróxima");
+        break
+    }
 }
+
